2ada07b2a2c7a4c763ed06cc37b4035de75a278e
Eunhye Kang <gracekang61@naver.com>
2022-03-17 00:31:07 +0900

    ch03_operator

---
+++ ch03/src/OperatorEx4.java
@@ -0,0 +1,12 @@
+
+public class OperatorEx4 { //부호연산자
+	public static void main(String[] args) {
+		int i= -10;
+		i = +i;
+		System.out.println(i);
+		
+		i= -10;
+		i = -i;
+		System.out.println(i);
+	}
+}
