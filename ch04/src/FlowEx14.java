6a71b8bf5abcc643e279562e743242c6f3a7bfd5
Eunhye Kang <gracekang61@naver.com>
2022-03-17 20:44:55 +0900

    ch04_for

---
+++ ch04/src/FlowEx14.java
@@ -0,0 +1,15 @@
+
+public class FlowEx14 {
+	public static void main(String[] args) {
+		//변수2
+		for(int i=1,j=10; i<=10; i++,j--) {
+			System.out.printf("%d\t%d%n",i,j);
+		}
+		System.out.println();
+		
+		//변수1개
+		for(int i=1; i<=10; i++) {
+			System.out.printf("%d\t%d%n",i,11-i);
+		}
+	}
+}
