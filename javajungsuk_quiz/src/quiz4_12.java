1d36a472334b8909155bd7d37de1cae2be88deac
Eunhye Kang <gracekang61@naver.com>
2022-03-25 03:35:20 +0900

    quiz_4_12_solve

--- javajungsuk_quiz/src/quiz4_12.java
+++ javajungsuk_quiz/src/quiz4_12.java
@@ -1,6 +1,23 @@
 
 public class quiz4_12 {
 	public static void main(String[] args) {
+		int x=2;
+		for(int k=0; k<3; k++,x+=3) {
+			
+			
+			for(int i=1; i<=3; i++) {
+				int count=0;
+				
+				for( int j=x; j<=9; j++) {
+					System.out.printf("%d*%d=%d\t",j,i,i*j);
+					count++;
+					if(count==3) {break;}
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+		/*
 		for(int i=1; i<=3; i++) {
 			int count=0;
 			for(int j=2; j<=9; j++) {
@@ -37,6 +54,8 @@ public class quiz4_12 {
 			System.out.println();
 		
 		}
+		*/
+		
 		
 	}
 }
