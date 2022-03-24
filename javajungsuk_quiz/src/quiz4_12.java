c49cff2313e5ad817e60b5ba45ab294ed7d4159c
Eunhye Kang <gracekang61@naver.com>
2022-03-25 03:17:13 +0900

    quiz_4_12_tryagain

--- javajungsuk_quiz/src/quiz4_12.java
+++ javajungsuk_quiz/src/quiz4_12.java
@@ -2,10 +2,37 @@
 public class quiz4_12 {
 	public static void main(String[] args) {
 		for(int i=1; i<=3; i++) {
+			int count=0;
 			for(int j=2; j<=9; j++) {
 				System.out.printf("%d*%d=%d\t",j,i,i*j);
-//				if(j==4 ||j==7)System.out.println();
+				count++;
 				
+				if(count==3)break;
+			}
+			System.out.println();
+		
+		}
+		System.out.println();
+		
+		for(int i=1; i<=3; i++) {
+			int count=0;
+			for(int j=5; j<=9; j++) {
+				System.out.printf("%d*%d=%d\t",j,i,i*j);
+				count++;
+				
+				if(count==3)break;
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(int i=1; i<=3; i++) {
+			int count=0;
+			for(int j=8; j<=9; j++) {
+				System.out.printf("%d*%d=%d\t",j,i,i*j);
+				count++;
+				
+				if(count==3)break;
 			}
 			System.out.println();
 		
