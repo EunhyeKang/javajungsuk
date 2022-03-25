d80eb802344bb0e94874d3b1f928a8f8488fd1ee
Eunhye Kang <gracekang61@naver.com>
2022-03-25 19:50:03 +0900

    quiz_ch06

---
+++ javajungsuk_quiz/src/quiz6_1.java
@@ -0,0 +1,26 @@
+
+public class quiz6_1 {
+	public static void main(String[] args) {
+		SutdaCard card1 = new SutdaCard(3,false);
+		SutdaCard card2 = new SutdaCard();
+		
+		System.out.println(card1.info());
+		System.out.println(card2.info());
+	}
+}
+
+class SutdaCard{
+	int num;
+	boolean isKwang;
+	
+	SutdaCard(int num, boolean b){
+		this.num = num;
+		this.isKwang = b;
+	}
+	SutdaCard(){
+		this(1,true);
+	}
+	String info(){
+		return num + (isKwang? "k" : "");
+	}
+}
---
+++ javajungsuk_quiz/src/quiz6_3.java
@@ -0,0 +1,50 @@
+
+public class quiz6_3 {
+	public static void main(String[] args) {
+		Student s = new Student(); 
+		s.name = "홍길동";
+		s.ban = 1;
+		s.no = 1;
+		s.kor = 100; s.eng = 60; s.math = 76;
+		
+		System.out.println("이름:"+s.name); 
+		System.out.println("총점:"+s.getTotal()); 
+		System.out.println("평균:"+s.getAverage());
+		
+		Student ss = new Student("강냉이",1,1,100,60,76);
+		System.out.println(ss.info());
+	}
+}
+
+class Student{
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+	
+	int getTotal(){
+		return kor+eng+math;
+	}
+	float getAverage(){
+		 
+		return (int)(getTotal()/3f*10+0.5)/10f;
+	}
+	Student(){
+		
+	}
+	Student(String name, int ban, int no, int kor, int eng, int math){
+		this.name = name;
+		this.ban = ban;
+		this.no = no;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	public String info(){
+		return name +","+ ban +","+ no +","+ kor
+				+","+eng + ","+math + ","+getTotal()+","+getAverage();
+	}
+	
+}
