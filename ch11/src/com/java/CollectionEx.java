d31dcf529add3e13e6f218541bb0d52b27c06729
Renee <gracekang61@naver.com>
2022-04-03 00:44:10 +0900

    collections method

---
+++ ch11/src/com/java/CollectionEx.java
@@ -0,0 +1,58 @@
+package com.java;
+
+import java.util.*;
+import static java.util.Collections.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionEx {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		System.out.println(list);
+		
+		addAll(list,1,2,3,4,5);
+		System.out.println(list);
+		
+		rotate(list,2);
+		System.out.println(list);
+		
+		swap(list,0,2);
+		System.out.println(list);
+		
+		shuffle(list);
+		System.out.println(list);
+		
+		sort(list);
+		System.out.println(list);
+		
+		sort(list,reverseOrder());
+		
+		int idx = binarySearch(list, 3);
+		System.out.println("index of 3 = "+idx);
+	
+		System.out.println("max="+max(list));
+		System.out.println("min="+min(list));
+		System.out.println("min="+max(list,reverseOrder()));
+		
+		fill(list,9);
+		System.out.println(list);
+		
+		List newList = nCopies(list.size(),2);
+		System.out.println(newList);
+		
+		System.out.println(disjoint(list,newList));//공통요소가 없으면 true
+		
+		copy(list,newList);
+		System.out.println("newList =" + newList);
+		System.out.println("list =" + newList);
+		
+		replaceAll(list,2,1);
+		System.out.println(list);
+		
+		Enumeration e = enumeration(list);
+		ArrayList list2 = list(e);
+		System.out.println("list2 =" + list2);
+		
+	}
+}
