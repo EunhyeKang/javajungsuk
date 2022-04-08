f579c5ab2148ee44997336262445f9d9e6a0a1ff
Renee <gracekang61@naver.com>
2022-04-08 17:16:50 +0900

    calendar_//

--- ch10/src/Ex10_2.java
+++ ch10/src/Ex10_2.java
@@ -12,15 +12,18 @@ public final class Ex10_2 {
 		Calendar date1 = Calendar.getInstance();
 		Calendar date2 = Calendar.getInstance();
 		
+		//month의 경우 0부터 시작하기 때문에 4월인 경우, 3로 지정해야한다.
+		//date1.set(2019,Calendar.APRIL,29);와 같이 할수도 있다.
 		date1.set(2019, 3, 29); 
 		
 		System.out.println("date1은 "+toString(date1));
 		System.out.println("오늘(date2)는 " + toString(date2) + DAY_OF_WEEK[date2.get(Calendar.DAY_OF_WEEK)]+"요일");
 		
+		//두 날짜간의 차이를 얻으려면, getTimeInMillis() 천분의 일초 단위로 변환해야 한다.
 		long diff = Math.abs( (date2.getTimeInMillis() - date1.getTimeInMillis())/1000 );
 		
 		System.out.println("date1부터 date2까지 "+ diff +"초가 지났습니다.");
-		System.out.println("일(day)로 계산하면 " + diff/(24*60*60)+"일 입니다.");
+		System.out.println("일(day)로 계산하면 " + diff/(24*60*60)+"일 입니다.");//1일 = 24시간 * 60분 * 60초
 		
 	}
 	
