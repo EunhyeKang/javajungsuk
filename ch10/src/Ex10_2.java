e518c064b8dcd640d5473c0251e98c6e8d0f68ec
Renee <gracekang61@naver.com>
2022-04-08 17:13:53 +0900

    calendar

---
+++ ch10/src/Ex10_2.java
@@ -0,0 +1,30 @@
+import java.util.Calendar;
+
+public final class Ex10_2 {
+	public static void main(String[] args) {
+		final String[] DAY_OF_WEEK= {"","일","월","화","수","목","금","토"};
+		
+//		date1 년월일 요일,
+//		오늘 date2 년원일 요일
+//		그날 date1-date2까지 초
+//		일로 계산하면 며칠.
+//		
+		Calendar date1 = Calendar.getInstance();
+		Calendar date2 = Calendar.getInstance();
+		
+		date1.set(2019, 3, 29); 
+		
+		System.out.println("date1은 "+toString(date1));
+		System.out.println("오늘(date2)는 " + toString(date2) + DAY_OF_WEEK[date2.get(Calendar.DAY_OF_WEEK)]+"요일");
+		
+		long diff = Math.abs( (date2.getTimeInMillis() - date1.getTimeInMillis())/1000 );
+		
+		System.out.println("date1부터 date2까지 "+ diff +"초가 지났습니다.");
+		System.out.println("일(day)로 계산하면 " + diff/(24*60*60)+"일 입니다.");
+		
+	}
+	
+	public static String toString(Calendar date) {
+		return date.get(Calendar.YEAR)+"년"+(date.get(Calendar.MONTH)+1)+"월"+date.get(Calendar.DATE)+"일";
+	}
+}
